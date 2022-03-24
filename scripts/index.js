e6ef04adf80862cc7bc925d8e96b7315836c8fb2
Afran <Jvelez13Soto@outlook.com>
2022-03-24 12:23:35 -0600

    Dashboard_addelements

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,26 @@
 var tog = 1;
 var tog1 = 1;
 var togindi = 0;
+
+const widgetIndicador = ` <div class="widget-item x">
+<i class="fa fa-gear" style="font-size: 22px"></i>
+<div class="indicator" id="indicator"></div>
+Lampara
+</div>`;
+
+const widgetswitch = ` <div class="widget-item x">
+<i class="fa fa-gear" style="font-size: 22px"></i>
+<div class="indicator" id="indicator"></div>
+switch
+</div>`;
+
+const widgetdimmer = ` <div class="widget-item x">
+<i class="fa fa-gear" style="font-size: 22px"></i>
+<div class="indicator" id="indicator"></div>
+dimmer
+</div>`;
+
+
 function myFunction(x) {
   x.classList.toggle("is-active");
   console.log(tog);
@@ -37,18 +57,44 @@ let trans = () => {
     document.documentElement.classList.remove("transition");
   }, 1000);
 };
+const widgetSidebar =  document.getElementById("widgets");
+const widgetSidebarprinter = document.getElementById("Impresoras&CNC");
+const widgetPanel =  document.getElementById("panel");
 
-const indicador =  document.getElementById("indicator");
-const widget =  document.getElementById("widget");
-widget.addEventListener("click", function () {
+Sortable.create(widgetPanel,{});
+
+
+window.addEventListener('load', (event) => {
+  widgetSidebar.childNodes.forEach((widget) => {
+    widget.addEventListener('click', (e) => {
+      if(e.target.id == 'Alarma'){
+        widgetPanel.innerHTML += widgetIndicador;
+      }
+      else if(e.target.id == 'Dimmer'){
+        widgetPanel.innerHTML += widgetdimmer;
+      }
+      else if(e.target.id == 'Switch'){
+        widgetPanel.innerHTML += widgetswitch;
+        
+      }
+    })
+  })
+  widgetSidebarprinter.childNodes.forEach((impresora) => {
+    impresora.addEventListener('click', (e) => {
+      console.log("agregar impresora");
+    })
+  })
+});
+
+
+function Indicador(x) {
   if (togindi === 1) {
-    indicador.style.backgroundColor = "rgb(224, 60, 60)";
-    indicador.style.boxShadow = " rgba(100, 87, 87, 0.2) 0 -1px 5px 1px, inset #c01f1f 0 -1px 9px, rgba(255, 0, 0, 0.5) 0 2px 12px";
+    x.style.backgroundColor = "rgb(224, 60, 60)";
+    x.style.boxShadow = " rgba(100, 87, 87, 0.2) 0 -1px 5px 1px, inset #c01f1f 0 -1px 9px, rgba(255, 0, 0, 0.5) 0 2px 12px";
     togindi = 0;
   } else {
-    indicador.style.backgroundColor = "rgb(27, 79, 221)";
-    indicador.style.boxShadow = "rgba(100, 87, 87, 0.2) 0 -1px 5px 1px, inset #1f52c0 0 -1px 9px, rgba(4, 0, 255, 0.815) 0 2px 12px";
+    x.style.backgroundColor = "rgb(27, 79, 221)";
+    x.style.boxShadow = "rgba(100, 87, 87, 0.2) 0 -1px 5px 1px, inset #1f52c0 0 -1px 9px, rgba(4, 0, 255, 0.815) 0 2px 12px";
     togindi = 1;
   }
-  
-});
+};
